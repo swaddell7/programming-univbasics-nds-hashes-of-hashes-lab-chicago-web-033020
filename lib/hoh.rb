fa9fe937b75904dd026259ed1dad96a0c867686f
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-11 20:18:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,20 @@ def naming_system
   sub_category: {
     label: "Phylum",
     sub_category: {
-      label: "Class"
-      s
+      label: "Class",
+      sub_category: {
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species"
+            }
+          }
+        }
+      }
+    }
+  }  
 }
 end
